f1a489f077e12851f98efe5768f85407a70d2b77
Henry Wittich <hwittich@luc.edu>
2021-03-18 12:10:47 -0500

    Cleaned up script 2 and fixed hard coded file paths

--- 02_make_bed.sh
+++ 02_make_bed.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 pop=$1 #stores first argument in variable pop
+vcf=$2 #second argument is the path to the directory directories for each population that contain vcf files
+out=$3 #third argument is the path to the directory for the output bed/bim/fam files
 for chr in {1..22} #Loop through chromosomes 1-22
 do
-	./plink --vcf /home/egeoffroy/MESA_TOPMED_Imputation/${pop}/chr${chr}.dose.vcf.gz --make-bed --out /home/egeoffroy/LD_matrix/${pop}/${pop}_chr${chr}_dose
+	./plink --vcf ${vcf}/${pop}/chr${chr}.dose.vcf.gz --make-bed --out ${out}/${pop}/${pop}_chr${chr}_dose
 	#--vcf flag specifies the vcf input files #1000 genomes genotype dosage data
 	#--make-bed reformats dosage files into .bed/bim/fam files #PLINK binary formats
 done

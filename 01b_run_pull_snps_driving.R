bc50d1637dad089605c3ab7b12b482e136857626
Henry Wittich <hwittich@luc.edu>
2021-03-24 21:29:47 -0500

    Added test command for script 1c

--- 01b_run_pull_snps_driving.R
+++ 01b_run_pull_snps_driving.R
@@ -1,8 +1,10 @@
 #Test command
 #Rscript 01b_run_pull_snps_driving.R 3 /homes/data/COLOC_input_data/uniq_pred_db_hg38.chrchrom.maf0.01.R20.8.anno.txt.gz /homes/data/COLOC_input_data/annotation_all_aptamers_ENSG.txt /homes/data/COLOC_input_data/uniq_pred_db_hg38.chrchrom.maf0.01.R20.8.geno.txt.gz /homes/data/COLOC_input_data/annotation_all_aptamers_ENSG.txt ASW AFA CEU
+#Running 01c
+#./01c_run_chrom.sh /homes/data/COLOC_input_data/uniq_pred_db_hg38.chrchrom.maf0.01.R20.8.anno.txt.gz /homes/data/COLOC_input_data/annotation_all_aptamers_ENSG.txt /homes/data/COLOC_input_data/uniq_pred_db_hg38.chrchrom.maf0.01.R20.8.geno.txt.gz /homes/data/COLOC_input_data/annotation_all_aptamers_ENSG.txt ASW AFA CEU
+
 source('01_pull_snps_driving.R') # this script pulls snps within 1Mb of the transcriptome start and end sites
 "%&%" <- function(a,b) paste(a,b, sep='')
-
 argv <- commandArgs(trailingOnly = TRUE)
 chrom <- argv[1]
 snp_annot_file <- argv[2]

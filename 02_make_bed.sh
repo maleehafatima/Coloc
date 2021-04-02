a92a869d9df04e12cbaa5e037a22232bb619b045
Henry Wittich <hwittich@luc.edu>
2021-04-02 14:28:53 -0500

    Adjusted file paths of script 2 while testing

--- 02_make_bed.sh
+++ 02_make_bed.sh
@@ -2,9 +2,12 @@
 
 pop=$1 #stores first argument in variable pop
 vcf=$2 #second argument is the path to the directory containing directories for each population that contain vcf files
-for chr in {1..22} #Loop through chromosomes 1-22
+#for chr in {1..22} #Loop through chromosomes 1-22
+for chr in {1..3} #for testing purpose
 do
-	./plink --vcf ${vcf}/${pop}/chr${chr}.dose.vcf.gz --make-bed --out output/LD_matrix/${pop}/${pop}_chr${chr}_dose
+	echo ${vcf}/${pop}.chr${chr}.phase3.vcf.gz
+	echo ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
+	plink --vcf ${vcf}/${pop}.chr${chr}.phase3.vcf.gz --make-bed --out ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
 	#--vcf flag specifies the vcf input files #1000 genomes genotype dosage data
 	#--make-bed reformats dosage files into .bed/bim/fam files #PLINK binary formats
 done

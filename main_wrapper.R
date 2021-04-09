db7f7c3d8b1305d5e6e731628b1109a655cc2c48
Annie M. Novak <anovak9@luc.edu>
2021-04-09 15:56:46 -0500

    Code to make directories in wrapper

--- main_wrapper.R
+++ main_wrapper.R
@@ -3,30 +3,50 @@ library(data.table)
 
 #For running script 1: check if they have the necessary directories otherwise make output -> LD_matrix -> folders for every population
 
-#Get & set working directory
+## Get & set working directory
+#Tell user to cd into Coloc before running
 wd <- getwd()
 wd <- paste(wd,'/',sep='')
 
-#Import necessary scripts
+
+## Import necessary scripts
 source(paste(wd,'05_coloc.R',sep=''))
 source(paste(wd, '05b_run_coloc.R', sep=''))
 
-#make output folder 
 
-#Import arguments 
+## Import arguments 
 args <- commandArgs(trailingOnly=TRUE)
 print(args)
-pops <- args[1]
-phenos <- args[2]
-eqtl <- args[3] #NOTE: probs don't need this argument
-gwas<- args[4] #NOTE: probs don't need this argument
-LD_dir<- args[5] #NOTE: probs don't need this argument
-gene_id<-args[6]
-pop_sizes<-as.numeric(args[7])
-ld_matrix<-args[8]
-gwas_size<-as.numeric(args[9]) #NOTE: probs don't need this argument
+input_dir <- args[1]
+pops_1 <- args[2]
+pops_4 <- args[3]
+phenos <- args[4]
+pop_sizes <- as.numeric(args[5])
+#gene_id<-args[6]  #NOTE: probs don't need this argument
 
 genes<-readRDS(gene_id) #NOTE: need to figure out how this works with 5b
+
+
+## MAKE DIRECTORIES
+#Create output dir
+dir.create(paste(wd, 'output', sep = ''), showWarnings = F)
+#List of output dirs
+dirs <- c('LD_matrix', 'gene_lists', 'GWAS_TOPMED', 'pQTL', 'Coloc_output')
+#Create output subdirs
+for(dir in dirs){
+  dir.create(paste(wd,'output/', dir, sep = ''), showWarnings = F)
+}
+#Create pop dirs in subdirs
+for(pop in pops_1){
+  dir.create(paste(wd,'output/LD_matrix/', pop, sep = ''), showWarnings = F)
+  dir.create(paste(wd,'output/LD_matrix/', pop, '/', pop, '_1Mb_coords_LDMatrix', sep = ''), showWarnings = F)
+  dir.create(paste(wd,'output/gene_lists/', pop, sep = ''), showWarnings = F)
+}
+for(pop in pops_4){
+  dir.create(paste(wd,'output/GWAS_TOPMED/', pop, sep = ''), showWarnings = F)
+  dir.create(paste(wd,'output/pQTL/', pop, sep = ''), showWarnings = F)
+}
+
 
 
 
@@ -34,10 +54,10 @@ genes<-readRDS(gene_id) #NOTE: need to figure out how this works with 5b
 
 #Call 5b as no hup in this script
 
-#(From script 5c) just becomes part of end of wrapper 
-for(i in 1:length(pops)){
-        for(pheno in phenos){
-                coloc_analysis(pop=as.character(pops[i]), pop_size=pop_sizes[i], phenotype=pheno, gene_id=genes, ld_matrix=ld_matrix)
-        }
+## Run COLOC analysis  
+for(i in 1:length(pop_sizes)){
+  for(pheno in phenos){
+    coloc_analysis(pop_1=as.character(pops_1[i]), pop_4=as.character(pops_4[i]), pop_size=pop_sizes[i], phenotype=pheno, gene_id=genes, ld_matrix=ld_matrix)
+  }
 }
 

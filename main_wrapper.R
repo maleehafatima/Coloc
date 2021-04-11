5c6488ab951926d4438aac085a7cdd3ec17baf90
Annie M. Novak <anovak9@luc.edu>
2021-04-11 12:47:11 -0500

    Formatting main wrapper

--- main_wrapper.R
+++ main_wrapper.R
@@ -9,11 +9,6 @@ wd <- getwd()
 wd <- paste(wd,'/',sep='')
 
 
-## Import necessary scripts
-source(paste(wd,'05_coloc.R',sep=''))
-source(paste(wd, '05b_run_coloc.R', sep=''))
-
-
 ## Import arguments 
 args <- commandArgs(trailingOnly=TRUE)
 print(args)
@@ -48,17 +43,45 @@ for(pop in pops_4){
   dir.create(paste(wd,'output/pQTL/', pop, sep = ''), showWarnings = F)
 }
 
+print('Directories created.')
+
+## Scripts 1
+
+#Get rid of 1c and make it part of wrapper
+#Just have chrom for loop and then call function from 1b
+#Reformat 1b: get rid of UNIX input arguments and instead make script a function with the UNIX arguments as parameters for function, for loop/main will be in function
+
+print('Pulling SNPs completed.')
+
+## Scripts 2
+
+#Could possibly make 2b part of main, or can keep it separate 
 
 
 
+## Script 3
+
+print('LD matrices created.')
 
 
-#Call 5b as no hup in this script
+## Script 4
 
-## Run COLOC analysis  
+source(paste(wd, '04_prep_files_coloc.R', sep = ''))
+prep_files(pops = as.character(pops_4), pop_sample_size = pop_sizes, phenos = phenos, chrs = chrs)
+
+print('Input files formatted.')
+
+
+## Scripts 5
+
+source(paste(wd,'05_coloc.R',sep=''))
+source(paste(wd, '05b_run_coloc.R', sep=''))
+
+##Call 5b as no hup in this script?
+#Run COLOC analysis  
 for(i in 1:length(pop_sizes)){
   for(pheno in phenos){
-    coloc_analysis(pop_1=as.character(pops_1[i]), pop_4=as.character(pops_4[i]), pop_size=pop_sizes[i], phenotype=pheno, gene_id=genes, ld_matrix=ld_matrix)
+    coloc_analysis(pop_1=as.character(pops_1[i]), pop_4=as.character(pops_4[i]), pop_size=pop_sizes[i], phenotype=pheno, gene_id=genes)
   }
 }
 

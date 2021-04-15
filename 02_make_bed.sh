0f85530ce011630b3b6e2064e84d18fbdc2021d6
Henry Wittich <hwittich@luc.edu>
2021-04-14 20:56:40 -0500

    Filter out individuals with more than 1% of genotypes missing and SNPs with a minor allele frequency below 1%

--- 02_make_bed.sh
+++ 02_make_bed.sh
@@ -8,7 +8,7 @@ for chr in {1..3} #for testing purpose
 do
 	#echo ${vcf}/${pop}.chr${chr}.phase3.vcf.gz
 	#echo ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
-	plink --vcf ${vcf}/${pop}.chr${chr}.phase3.formatted.vcf.gz --make-bed --out ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
+	plink --vcf ${vcf}/${pop}.chr${chr}.phase3.formatted.vcf.gz --make-bed --mind 0.01 --maf 0.01 --out ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
 	#--vcf flag specifies the vcf input files #1000 genomes genotype dosage data
 	#--make-bed reformats dosage files into .bed/bim/fam files #PLINK binary formats
 done

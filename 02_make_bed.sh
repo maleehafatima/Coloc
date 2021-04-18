2ce962f5c5d502ce358d0c0915faff3f7956ea48
Henry Wittich <hwittich@luc.edu>
2021-04-17 19:30:02 -0500

    Added file recognition functionality so that the script finds the correct vcf file in the directory based on the chromosome inputted. Vcf files must be formatted 'chr*.'

--- 02_make_bed.sh
+++ 02_make_bed.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 pop=$1 #stores first argument in variable pop
-vcf=$2 #second argument is the path to the directory containing directories for each population that contain vcf files
+chr=$2 #second argument is the chromosome number
+vcf=$3 #third argument is the path to the directory containing directories for each population that contain vcf files
 
-#for chr in {1..22} #Loop through chromosomes 1-22
-for chr in {1..3} #for testing purpose
+for file in ${vcf}/*
 do
-	#echo ${vcf}/${pop}.chr${chr}.phase3.vcf.gz
-	#echo ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
-	plink --vcf ${vcf}/${pop}.chr${chr}.phase3.formatted.vcf.gz --make-bed --mind 0.01 --maf 0.01 --out ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
-	#--vcf flag specifies the vcf input files #1000 genomes genotype dosage data
-	#--make-bed reformats dosage files into .bed/bim/fam files #PLINK binary formats
+	filename="${file#"${vcf}"}"
+	if grep -q "chr"$chr"\." <<< "$filename"; then
+		#Run plink command on correct vcf file
+		plink --vcf ${file} --make-bed --mind 0.01 --maf 0.01 --out ${PWD}/output/LD_matrix/${pop}/${pop}_chr${chr}_dose
+		#--vcf flag specifies the vcf input files #1000 genomes genotype dosage data
+		#--make-bed reformats dosage files into .bed/bim/fam files #PLINK binary formats
+	fi
 done

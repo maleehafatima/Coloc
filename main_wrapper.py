63c08dbf27e24771a63b1f90fe63755cf6fcca52
annie-novak9 <anovak9@luc.edu>
2021-04-20 16:03:15 -0500

    Update main_wrapper.py
    Pipeline only runs one pop at a time

--- main_wrapper.py
+++ main_wrapper.py
@@ -16,13 +16,12 @@ parser.add_argument('--geno', required=True, help = 'genotype files directory pa
 parser.add_argument('--expr', required=True, help = 'Exact expression file path')
 parser.add_argument('--frq', required = True, help = 'Exact frq file path')
 parser.add_argument('--out', required=True, help = 'Specify main output directory for all output file')
-parser.add_argument('--pops1', action = 'extend', required=True, help = 'Populations used for vcf files')
-parser.add_argument('--pops4', action = 'extend', required=True, help = 'Populations used for frq and eQTL files')
-parser.add_argument('--pop_sizes', type = int, action = 'extend', required=True, help = 'Size of populations, in respective order to pop list')
+parser.add_argument('--pop1', required=True, help = 'Populations used for vcf files')
+parser.add_argument('--pop4', required=True, help = 'Populations used for frq and eQTL files')
+parser.add_argument('--pop_size', type = int, action = 'extend', required=True, help = 'Size of populations, in respective order to pop list')
 parser.add_argument('--phenotypes', action = 'extend', required=True, help = 'Phenotypes to test')
 parser.add_argument('--chrs', type = int, action = 'extend', required=True, help = 'Indicate what chromosomes to run')
 #Flag to specify whether to run all genes or genes specified by user
-#Maybe make default -1?
 parser.add_argument('-gene_id', type = str, action = 'extend', nargs='?', default= False, help = 'Input specific genes ids to run colocalization on, default is set to False')
 args = parser.parse_args()
 
@@ -39,14 +38,14 @@ geno = args.geno
 expr = args.expr
 frq = args.frq
 out = args.out
-pops1 = args.pops1
-pops4 = args.pops4
-pop_sizes = args.pop_sizes
+pop1 = args.pop1
+pop4 = args.pop4
+pop_size = args.pop_size
 phenos = args.phenotypes
 chrs = args.chrs
 #Convert lists from int to str
 chrs = map(str, chrs)
-pop_sizes = map(str, pop_sizes)
+pop_size = str(pop_size)
 #Determine if user input list of genes
 if args.gene_id != False:
     gene_ids = args.gene_id
@@ -64,24 +63,22 @@ for dir in dirs:
     if os.path.isdir(temp_path) == False:
         os.mkdir(temp_path)
 #Make population directories in subdirectories 
-for pop in pops1:
-    temp_path = path + 'LD_matrix/' + pop
-    if os.path.isdir(temp_path) == False:
-        os.mkdir(temp_path)
-    temp_path = temp_path + '/' + pop + '_1Mb_coords_LDMatrix'
-    if os.path.isdir(temp_path) == False:
-        os.mkdir(temp_path)
-    temp_path = path + 'gene_lists/' + pop
-    if os.path.isdir(temp_path) == False:
-        os.mkdir(temp_path)
+temp_path = path + 'LD_matrix/' + pop1
+if os.path.isdir(temp_path) == False:
+    os.mkdir(temp_path)
+temp_path = temp_path + '/' + pop1 + '_1Mb_coords_LDMatrix'
+if os.path.isdir(temp_path) == False:
+    os.mkdir(temp_path)
+temp_path = path + 'gene_lists/' + pop1
+if os.path.isdir(temp_path) == False:
+    os.mkdir(temp_path)
 #Make population directories in subdirectories 
-for pop in pops4:
-    temp_path = path + 'GWAS_TOPMED/' + pop
-    if os.path.isdir(temp_path) == False:
-        os.mkdir(temp_path)
-    temp_path = path + 'eQTL/' + pop
-    if os.path.isdir(temp_path) == False:
-        os.mkdir(temp_path)
+temp_path = path + 'GWAS_TOPMED/' + pop4
+if os.path.isdir(temp_path) == False:
+    os.mkdir(temp_path)
+temp_path = path + 'eQTL/' + pop4
+if os.path.isdir(temp_path) == False:
+    os.mkdir(temp_path)
 #Save LD ouput dir
 ld = path + 'LD_matrix/'
 
@@ -92,22 +89,30 @@ Run Scripts
 '''
 ## Run scripts w/specified gene list
 if args.gene_id != False:
-    for i in len(pops1):
-        for pheno in phenos:
-            x = 1 #random line to get rid of error for now
+    for pheno in phenos:
+        x = 1 #random line to get rid of error for now
 
 ## Run all genes in chromosomes
 else:
-    for i in len(pops1):
-        
-        pop = pops1[i]
-        pop4 = pops4[i]
-        pop_size = pop_sizes[i]
-        
-        for pheno in phenos:
+    for pheno in phenos:
             
-            print('Run ' + pop + ' for ' + pheno + '.')
+        print('Run ' + pop1 + ' for ' + pheno + '.')
 
+<<<<<<< HEAD
+        ## Script 2
+        for chr in chrs:
+            #maybe implement subprocess for parallelization
+            os.system("chmod u+x 02_make_bed.sh") #Make the script executable
+            cmd = "./02_make_bed.sh "+pop1+" "+vcf
+            os.system(cmd)
+        print('Bfiles made.')
+
+        ## Scripts 1
+        for chr in chrs:
+            script1cmd = 'Rscript 01b_run_pull_snps_driving.R ' + chr + ' ' + snp_annot + ' ' + gene_annot + ' ' + geno \
+                    + ' ' + expr + ' ' + pop1 + ' > output/LD_matrix/nohup_1Mb_chrom' + chr + '_2.out &'
+            os.system(script1cmd)
+=======
             ## Script 2
             for chr in chrs:
                 #maybe implement subprocess for parallelization
@@ -137,80 +142,59 @@ else:
                 script1cmd = 'nohup Rscript 01b_run_pull_snps_driving.R ' + chr + ' ' + chr_snp + ' ' + chr_gene + ' ' + chr_geno \
                     + ' ' + chr_expr + ' ' + pop + ' > output/LD_matrix/nohup_1Mb_chrom' + chr + '_2.out &'
                 os.system(script1cmd)
+>>>>>>> 4e57eba44dc97f8ce5b4e639be11037dc30f544d
 
-            print('Pulling SNPs completed.')
+        print('Pulling SNPs completed.')
 
-            ## Script 3
-            for chr in chrs:
-                os.system("chmod u+x 03_make_LD_matrix.sh")
-                os.system("./03_make_LD_matrix.sh "+pop+" "+chr)
+        ## Script 3
+        for chr in chrs:
+            os.system("chmod u+x 03_make_LD_matrix.sh")
+            os.system("./03_make_LD_matrix.sh "+pop1+" "+chr)
 		    
-            print('LD matrices created.')
+        print('LD matrices created.')
 
 
-            ## Script 4
-            '''
-            Command line arguments needed for Rscript:
-            gwas <- args[1] #specify exact file path of gwas sum stat for this pheno in wrapper command
-            pqtl <- args[2] #specify exact file path in wrapper command for this pop
-            frq <- args[3] #not sure if this will work, need to specify exact file path in wrapper command
-            out <- args [4] #output directory
-            pop <- args[5]
-            pop_size <- args[6]
-            pheno <- args[7]
-            chrs <- tail(args, -7)
-            '''
-            #Get list of files in gwas directory
-            gwas_files = os.listdir(gwas)
-            #Get gwas file specific to phenotype
-            for file in gwas_files:
-                if pheno in file:
-                    pheno_gwas_file = file
-            #Run script 4 command
-            chrs_unlist = ' '.join(chrs)
-            cmd = 'Rscript '+ wd + '/04_prep_files_coloc.R '+ gwas + '/' + pheno_gwas_file + ' ' + eqtl + ' ' + frq + ' ' + out \
+        ## Script 4
+       
+        #Get list of files in gwas directory
+        gwas_files = os.listdir(gwas)
+        #Get gwas file specific to phenotype
+        for file in gwas_files:
+            if pheno in file:
+                pheno_gwas_file = file
+        #Run script 4 command
+        chrs_unlist = ' '.join(chrs)
+        cmd = 'Rscript 04_prep_files_coloc.R '+ gwas + '/' + pheno_gwas_file + ' ' + eqtl + ' ' + frq + ' ' + out \
                     + ' ' + pop4 + ' ' + pop_size + ' ' + pheno + ' ' + chrs_unlist
-            os.system(cmd)
+        os.system(cmd)
 
-            print('Input files formatted.')
+        print('Input files formatted.')
 
 
-            ## Scripts 5
-            #call Rscript as nohup
-            '''
-            Command line arguments needed for Rscript: 
-            gwas <- args[1] #specify exact file path of formatted gwas file in wrapper for this pop & phenotype
-            pqtl <- args[2] #specify exact file path of formatted pqtl file in wrapper for this pop & phenotype
-            ld <- args[3] #specify directory for ld matrices for this pop (aka pop_1Mb_coords_LDMatrix)
-            out <- args [4] #output directory
-            pop <- args[5]
-            pop_size <- args[6]
-            pheno <- args[7]
-            #Way to do conditional tail argument?
-            genes <- tail(args,-7)
-            '''
-            #Get formatted gwas file specific to pop and phenotype
-            out_gwas = out + '/GWAS_TOPMED/' + pop4 
-            gwas_files = os.listdir(out_gwas)
-            for file in gwas_files:
-                if pheno in file:
-                    if pop4 in file: 
-                        pheno_pop_gwas = file
-            #Get formatted eqtl file specific to pop & phenotype
-            out_eqtl = out + '/eQTL/' + pop4
-            eqtl_files = os.listdir(out_eqtl)
-            for file in eqtl_files:
-                if pheno in file:
-                    if pop4 in file:
-                        pheno_pop_eqtl = file
-            #Get LD dir for this pop
-            pop_ld = ld + pop + '/' + pop + '_1Mb_coords_LDMatrix'
-            #Run script 5 command
-            cmd = 'nohup Rscript ' + wd + '/05b_run_coloc.R' + pheno_pop_gwas + ' ' + pheno_pop_eqtl + ' ' + pop_ld + ' ' + out \
-                    + ' ' + pop + ' ' + pop_size + ' ' + pheno
-            os.system(cmd)
+        ## Scripts 5
+        
+        #Get formatted gwas file specific to pop and phenotype
+        out_gwas = out + '/GWAS_TOPMED/' + pop4 
+        gwas_files = os.listdir(out_gwas)
+        for file in gwas_files:
+            if pheno in file:
+                if pop4 in file: 
+                    pheno_pop_gwas = file
+        #Get formatted eqtl file specific to pop & phenotype
+        out_eqtl = out + '/eQTL/' + pop4
+        eqtl_files = os.listdir(out_eqtl)
+        for file in eqtl_files:
+            if pheno in file:
+                if pop4 in file:
+                    pheno_pop_eqtl = file
+        #Get LD dir for this pop
+        pop_ld = ld + pop1 + '/' + pop1 + '_1Mb_coords_LDMatrix'
+        #Run script 5 command
+        cmd = 'Rscript 05b_run_coloc.R' + pheno_pop_gwas + ' ' + pheno_pop_eqtl + ' ' + pop_ld + ' ' + out \
+                    + ' ' + pop1 + ' ' + pop_size + ' ' + pheno
+        os.system(cmd)
 
-            print('Coloc analysis finished')
+        print('Coloc analysis finished')
 
 
 print('Pipeline completed running.')
